755f7f558cfc5a559e84507018fbf7ac652a34a0
marLindaT <26019544+marLindaT@users.noreply.github.com>
2019-03-01 22:28:46 -0500

    Add files via upload

--- listWork.py
+++ listWork.py
@@ -1,4 +1,5 @@
 import csv
+import re
 
 
 ## inputFile = str(input("What is the input file?:"))
@@ -17,18 +18,47 @@ import csv
 ##put all oil lots into a list
 def getOilLotCodes(inputFile="workOrders"):
   "collect all lot numbers of a given oil production into a list"
-  lotType = str(input("What lot/strain type?:"))
+  lotType = 'Oil Black Widow'
+##  lotType = str(input("What lot/strain type?:"))
   with open(inputFile+".csv", "r") as sourceFile:
     csv_reader = csv.reader(sourceFile, delimiter=',')
     lotList = []
     for line in csv_reader:
-      if line[5] == lotType and len(line[3])<7:
-        if line[3] not in lotList:
-          lotList.append(line[3])
+      if line[5] == lotType and len(line[3])<8:
+      	
+      	q = re.search(r"^\d{4}(?!\d)", line[3])
+      	if q != None:
+      		if q.group() not in lotList:
+      			lotList.append(q.group())
+      		
+      	q =re.search(r"^\d{3}(?!\d)", line[3])
+      	if q != None:
+      		if q.group() not in lotList:
+      			lotList.append(q.group())
+    for i in lotList:
+    	float(i)
     
+    lotList.sort()         
   return lotList
 
-print(getOilLotCodes())
+lotList = getOilLotCodes()
+print("LOT LIST BELOW")
+print(lotList)
+
+
+def getWOInput(inputFile="workOrders", lotList=lotList, workOrderType="Oil - Milling"):
+  "given a list of lot numbers return a list of equal len/order of input mass in grams"
+  
+  with open(inputFile+".csv", "r") as sourceFile:
+    csv_reader = csv.reader(sourceFile, delimiter=',')
+    inputList = []
+    
+    
+                                  
+  return None
+
+print(getWOInput())
+    
       
 
     
