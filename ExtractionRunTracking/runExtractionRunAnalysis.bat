6e812e1203aa6f418bcd30768fb23a6b632f2d6e
marLindaT <26019544+marLindaT@users.noreply.github.com>
2019-04-09 16:42:04 -0400

    Add files via upload

---
+++ ExtractionRunTracking/runExtractionRunAnalysis.bat
@@ -0,0 +1,3 @@
+ECHO OFF
+C:\Users\Marshall\AppData\Local\Continuum\anaconda3\python.exe C:\Users\Marshall\Desktop\python-master\fileIO\processTracker\ExtractionRunTracking\extractionRunResultAnalysis.py
+pause
